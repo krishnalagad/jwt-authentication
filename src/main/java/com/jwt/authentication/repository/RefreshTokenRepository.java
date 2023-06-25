1c8a0cd471a6c82135b3cd8ab515a1a5856b4e38
Krishna Lagad <krishnalagad2@gmail.com>
2023-06-25 21:54:20 +0530

    Update RefreshTokenRepository.java

--- src/main/java/com/jwt/authentication/repository/RefreshTokenRepository.java
+++ src/main/java/com/jwt/authentication/repository/RefreshTokenRepository.java
@@ -4,4 +4,5 @@ import com.jwt.authentication.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+    
 }
